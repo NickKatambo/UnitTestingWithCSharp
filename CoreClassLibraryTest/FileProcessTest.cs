21c84858f8b1834d1d7f71df35e93ebf61c85451
Yannick Katambo <yannick.katambo@outlook.com>
2021-04-24 19:42:57 +0200

    Initial commit

--- CoreClassLibraryTest/FileProcessTest.cs
+++ CoreClassLibraryTest/FileProcessTest.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoreClassLibrary;
+using System;
 
 namespace CoreClassLibraryTest
 {
@@ -9,25 +10,62 @@ namespace CoreClassLibraryTest
         [TestMethod]
         public void FileNameDoesExist()
         {
-            Assert.Inconclusive();
+            //Arrange
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            //Act
+            fromCall = fp.FileExists(@"C:\Yann\Learning\CSharp\UnitTestingWithCSharp\TestFileCSharp.txt");
+
+            //Assert
+            Assert.IsTrue(fromCall);
+
         }
 
         [TestMethod]
         public void FileNameDoesNotExist()
         {
-            Assert.Inconclusive();
+            //Arrange
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            //Act
+            fromCall = fp.FileExists(@"C:\Yann\Learning\CSharp\UnitTestingWithCSharp\CSharp.txt");
+
+            //Assert
+            Assert.IsFalse(fromCall);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty_UsingAttribute()
         {
-            Assert.Inconclusive();
+            //Arrange
+            FileProcess fp = new FileProcess();
+
+            //Assert
+            fp.FileExists("");
         }
 
         [TestMethod]
         public void FileNameNullOrEmpty_UsingTryCatch()
         {
-            Assert.Inconclusive();
+            // Arrange
+            FileProcess fp = new FileProcess();
+
+            // Act
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                // Test was a success
+                return;
+            }
+
+            // Fail the test
+            Assert.Fail("Call to FileExists() did NOT throw an ArgumentNullException");
         }
     }
 }
